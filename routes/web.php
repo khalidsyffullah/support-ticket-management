fb63ee003b3b9fcedb7c48a9a81dc2b0c695e88f
KhalidSaiffullah <129648247+KhalidSaiffullah@users.noreply.github.com>
2025-08-15 03:55:35 +0600

    add config clear route

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\InstallController;
 
 /*
@@ -852,3 +853,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
 });
 
+Route::get('/clear-all', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('optimize:clear');
+    Artisan::call('queue:clear');
+    return "All caches cleared!";
+});

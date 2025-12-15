1a824a29c747e316ad8233f510c30d369010d963
KhalidSaiffullah <129648247+KhalidSaiffullah@users.noreply.github.com>
2025-12-16 03:14:51 +0600

    update build

--- database/seeders/EmailTemplateSeeder.php
+++ database/seeders/EmailTemplateSeeder.php
@@ -25,7 +25,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'create_ticket_new_customer',
             'details' => 'When customer create a new ticket from the landing page',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
 
         $html = File::get(public_path('html/email_templates/create_ticket_from_dashboard.html'));
@@ -34,7 +34,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'create_ticket_dashboard',
             'details' => 'When a ticket created from the admin page',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
 
         $html = File::get(public_path('html/email_templates/custom_mail.html'));
@@ -43,7 +43,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'custom_mail',
             'details' => 'It will use to send any custom email.',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
 
         $html = File::get(public_path('html/email_templates/ticket_assigned_user.html'));
@@ -52,7 +52,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'assigned_ticket',
             'details' => 'When a user got assigned for a ticket.',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
 
         $html = File::get(public_path('html/email_templates/ticket_updated.html'));
@@ -61,7 +61,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'ticket_updated',
             'details' => 'When a ticket has been updated.',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
 
         $html = File::get(public_path('html/email_templates/ticket_new_comment.html'));
@@ -70,7 +70,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'ticket_new_comment',
             'details' => 'When a comment has been added on a ticket.',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
 
         $html = File::get(public_path('html/email_templates/user_created.html'));
@@ -79,7 +79,7 @@ class EmailTemplateSeeder extends Seeder
             'slug' => 'user_created',
             'details' => 'When a new user created.',
             'language' => 'en',
-            'html' => str_replace('https://res.cloudinary.com/robinbd/image/upload/v1663394454/mail-template/help-desk-logo.png', $logo, $html)
+            'html' => str_replace('http://support.bdccl.gov.bd/images/logo.png', $logo, $html)
         ]);
         //
     }
